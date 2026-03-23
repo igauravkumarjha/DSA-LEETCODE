1e67ea9f07c6d2e56c038f0e875347df44dbe3f5
Gaurav kumar jha <igauravkumarjha@gmail.com>
2026-03-23 09:59:46 +0530

    Time: 0 ms (100.00%) | Memory: 24.6 MB (91.12%) - LeetSync

--- 3324-split-the-array/split-the-array.cpp
+++ 3324-split-the-array/split-the-array.cpp
@@ -4,17 +4,17 @@ public:
          sort(nums.begin(), nums.end());
        int z=(nums.size())/2;
         int g=nums.size();
-        vector<int> nums1;
-        vector<int> nums2;
+        // vector<int> nums1;
+        // vector<int> nums2;
 
         int i;
     
-        for( i=0; i<z;i++){
-            nums1.push_back(nums[i]);
-        }
-        for(int n=z; n<g; n++){
-             nums2.push_back(nums[n]);
-        }
+        // for( i=0; i<z;i++){
+        //     nums1.push_back(nums[i]);
+        // }
+        // for(int n=z; n<g; n++){
+        //      nums2.push_back(nums[n]);
+        // }
        
         int count = 1;
 
@@ -29,12 +29,7 @@ for(int l = 1; l < g; l++){
         
     }
 }
-        // if(nums1==nums2){
-        //     return false;
-        // }
-        // else{
-        //     return true;
-        // }
+        
          return true;
         
     }
