e766f31e161a36675060740148e140f28139aafb
Gaurav kumar jha <igauravkumarjha@gmail.com>
2026-03-23 09:56:52 +0530

    Time: 0 ms (100.00%) | Memory: 24.7 MB (87.79%) - LeetSync

---
+++ 3324-split-the-array/split-the-array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isPossibleToSplit(vector<int>& nums) {
+         sort(nums.begin(), nums.end());
+       int z=(nums.size())/2;
+        int g=nums.size();
+        vector<int> nums1;
+        vector<int> nums2;
+
+        int i;
+    
+        for( i=0; i<z;i++){
+            nums1.push_back(nums[i]);
+        }
+        for(int n=z; n<g; n++){
+             nums2.push_back(nums[n]);
+        }
+       
+        int count = 1;
+
+for(int l = 1; l < g; l++){
+    if(nums[l] == nums[l-1]){
+        count++;
+        if(count > 2){
+            return false;
+        }
+    } else {
+        count = 1;
+        
+    }
+}
+        // if(nums1==nums2){
+        //     return false;
+        // }
+        // else{
+        //     return true;
+        // }
+         return true;
+        
+    }
+};
