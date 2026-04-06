a8c38581d1ddfb92ffb7a4b4a69044a0eb999c5f
Gaurav kumar jha <igauravkumarjha@gmail.com>
2026-04-07 00:08:16 +0530

    Time: 3 ms (84.68%) | Memory: 9.7 MB (91.59%) - LeetSync

---
+++ 38-count-and-say/count-and-say.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        string res = "1";  // base case
+
+        for(int i = 2; i <= n; i++) {
+            string temp = "";
+            int count = 1;
+
+            for(int j = 1; j < res.size(); j++) {
+                if(res[j] == res[j - 1]) {
+                    count++;
+                } else {
+                    temp += to_string(count) + res[j - 1];
+                    count = 1;
+                }
+            }
+
+            // last group
+            temp += to_string(count) + res.back();
+
+            res = temp;
+        }
+
+        return res;
+    }
+};
